a014dd0d102e590378a21e7ac7d27a320633aca1
Jakub Onderka <jakub.onderka@gmail.com>
2014-04-25 09:47:02 +0200

    Actualized examples

--- examples/snippet.php
+++ examples/snippet.php
@@ -2,9 +2,9 @@
 use JakubOnderka\PhpConsoleColor\ConsoleColor;
 use JakubOnderka\PhpConsoleHighlighter\Highlighter;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $highlighter = new Highlighter(new ConsoleColor());
 
-$fileContent = file_get_contents(__DIR__ . '/example.php');
-echo $highlighter->getWholeFile($fileContent);
+$fileContent = file_get_contents(__FILE__);
+echo $highlighter->getCodeSnippet($fileContent, 3);
