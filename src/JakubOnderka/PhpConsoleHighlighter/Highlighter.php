7daa75df45242c8d5b75a22c00a201e7954e4fb5
Jakub Onderka <jakub.onderka@gmail.com>
2015-04-20 20:58:01 +0200

    Changed 'white' color for default token to 'default' color
    Looks better on terminal with white background

--- src/JakubOnderka/PhpConsoleHighlighter/Highlighter.php
+++ src/JakubOnderka/PhpConsoleHighlighter/Highlighter.php
@@ -22,7 +22,7 @@ class Highlighter
         self::TOKEN_STRING => 'red',
         self::TOKEN_COMMENT => 'yellow',
         self::TOKEN_KEYWORD => 'green',
-        self::TOKEN_DEFAULT => 'white',
+        self::TOKEN_DEFAULT => 'default',
         self::TOKEN_HTML => 'cyan',
 
         self::ACTUAL_LINE_MARK  => 'red',
